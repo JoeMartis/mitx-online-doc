3119d7ada413c9280d1ab04f16dfb54bdfc0dba2
JoeMartis <jmartis@mit.edu>
2022-04-08 09:52:32 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'JoeMama'
+copyright = '2021, Joe'
+author = 'Joe'
 
 release = '0.1'
 version = '0.1.0'
